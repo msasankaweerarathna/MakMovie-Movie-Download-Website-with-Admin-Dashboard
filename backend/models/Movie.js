3c53e932a181bf5cfbd8db6ff4a1650400ef078e
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-14 11:38:50 +0530

    Add Movie model schema for MongoDB with Mongoose
    - Created `MovieSchema` using Mongoose to define the structure of movie documents in the database.
    - Added various fields to the schema, including title, slug, posters, category, rating, genre, language, and more.
    - Included support for multiple download link qualities (480p, 720p, 1080p, 4K).
    - Enabled timestamps to automatically manage `createdAt` and `updatedAt` fields.
    - Exported the `Movie` model, ensuring it doesn't get redefined if it already exists.

---
+++ backend/models/Movie.js
@@ -0,0 +1,36 @@
+const { Schema } = require("mongoose");
+
+// create a model for the database
+const MovieSchema = new Schema(
+  {
+    title: { type: String },
+    slug: { type: String, require: true },
+    bgPoster: { type: String },
+    smPoster: { type: String },
+    titlecategory: { type: String },
+    description: { type: String },
+    rating: { type: String },
+    duration: { type: String },
+    year: { type: String },
+    genre: [{ type: String }],
+    language: { type: String },
+    subtitle: { type: String },
+    size: { type: String },
+    quality: { type: String },
+    youtubelink: { type: String },
+    category: { type: String },
+    watchonline: { type: String },
+    downloadlink: {
+      "480p": { type: String },
+      "720p": { type: String },
+      "1080p": { type: String },
+      "4K": { type: String },
+    },
+    status: { type: String },
+  },
+  {
+    timestamps: true, //this option will auto createdAT and UpdatedAt fields
+  }
+);
+
+export const Movie = models.Movie || model("Movie", MovieSchema, "movies");
