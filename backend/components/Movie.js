0c9e4f7e46cfc5364b009ac8f2464dfcba982fe9
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-19 23:16:44 +0530

    Enhance Movie component with slug generation, status, posters, and movie attributes
    - Implemented slug generation function to replace spaces with hyphens dynamically.
    - Added radio buttons for movie status (Draft/Publish).
    - Integrated input fields for main poster, slug URL, release year, YouTube link, language, quality, subtitle, and movie size.
    - Added radio buttons for movie title category and movie category selection.
    - Implemented checkboxes for selecting multiple movie genres.

--- backend/components/Movie.js
+++ backend/components/Movie.js
@@ -57,6 +57,27 @@ export default function Movie() {
     }));
   };
 
+  //Slug function, url for every space will be genarate - for every time press space
+  const handleSlugChange = (ev) => {
+    const inputValue = ev.target.value;
+
+    //Replace space with hyphens
+    const newSlug = inputValue.replace(/\s+/g, "-");
+
+    setSlug(newSlug);
+  };
+
+  //movie category
+  const categories = [
+    "Bollywood",
+    "Hollywood",
+    "South",
+    "Gujarati",
+    "Marvel_Studio",
+    "Tv_Shows",
+    "Web_Series",
+  ];
+
   return (
     <>
       <Head>
@@ -227,9 +248,224 @@ export default function Movie() {
             </div>
 
             {/* Movie status (Draft of Publish) */}
-            
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="status">Status</label>
+              <div className="flex gap-05">
+                <input
+                  type="radio"
+                  id="draft"
+                  name="status"
+                  value="draft"
+                  checked={status === "draft"}
+                  onChange={(e) => setStatus(e.target.value)}
+                />
+                <label htmlFor="draft">Draft</label>
+              </div>
+
+              <div className="flex gap-05">
+                <input
+                  type="radio"
+                  id="publish"
+                  name="status"
+                  value="publish"
+                  checked={status === "publish"}
+                  onChange={(e) => setStatus(e.target.value)}
+                />
+                <label htmlFor="publish">Publish</label>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-50 flex flex-col flex-left">
+            {/* Movie small poster */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="smposter">Main Poster</label>
+              <input
+                type="text"
+                id="smposter"
+                placeholder="smposter image link"
+                value={smPoster}
+                onChange={(e) => setSmposter(e.target.value)}
+              />
+            </div>
+
+            {/* Movie slug url */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="slug">Slug (url)</label>
+              <input
+                type="text"
+                id="slug"
+                placeholder="url of the movie"
+                value={slug}
+                onChange={handleSlugChange}
+              />
+            </div>
+
+            {/* Release year of the movie */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="year">Release Year</label>
+              <input
+                type="text"
+                id="year"
+                placeholder="year"
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+              />
+            </div>
+
+            {/* Youtube embed link */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="youtubelink">Youtube Link</label>
+              <input
+                type="text"
+                id="youtubelink"
+                placeholder="youtubelink"
+                value={youtubelink}
+                onChange={(e) => setYoutubelink(e.target.value)}
+              />
+            </div>
+
+            {/* language of the movie */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="language">Movie Language</label>
+              <select
+                onChange={(e) => setLanguage(e.target.value)}
+                value={language}
+                name="language"
+                id="language"
+              >
+                <option value="">Select Language</option>
+                <option value="Hindi (ORG)">Hindi (ORG)</option>
+                <option value="English">English</option>
+                <option value="Hindi - English">Hindi (ORG)</option>
+                <option value="Dual Audio [Hindi (ORG) + English]">
+                  Dual Audio [Hindi (ORG) + English]
+                </option>
+                <option value="Dual Audio [Hindi (Cleaned) + English]">
+                  Dual Audio [Hindi (Cleaned) + English]
+                </option>
+              </select>
+            </div>
+
+            {/* Quality of the movie */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="quality">Movie Quality</label>
+              <select
+                onChange={(e) => setQuality(e.target.value)}
+                value={quality}
+                name="quality"
+                id="quality"
+              >
+                <option value="">Select Quality</option>
+                <option value="480p || 720p || 1080p - WEB-DL">
+                  480p || 720p || 1080p - WEB-DL
+                </option>
+                <option value="480p || 720p || 1080p || 2160p - WEB-DL">
+                  480p || 720p || 1080p || 2160p - WEB-DL
+                </option>
+                <option value="480p || 720p || 1080p - HDTC">
+                  480p || 720p || 1080p - HDTC
+                </option>
+              </select>
+            </div>
+
+            {/* subtitle of the movie */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="subtitle">Movie Subtitle</label>
+              <select
+                onChange={(e) => setSubtitle(e.target.value)}
+                value={subtitle}
+                name="quality"
+                id="quality"
+              >
+                <option value="">Select Subtitle</option>
+                <option value="Hindi">Hindi</option>
+                <option value="English">English</option>
+              </select>
+            </div>
+
+            {/* Size of the movie */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+              <label htmlFor="size">Size of the movie</label>
+              <input
+                type="text"
+                id="size"
+                placeholder="350MB || 1GB || 2GB || 4GB"
+                value={size}
+                onChange={(e) => setSize(e.target.value)}
+              />
+            </div>
+
+            <div className="moviecategory flex flex-sb flex-left">
+              {/* movie title category */}
+              <div className="w-50 flex flex-col flex-left mb-2">
+                <label>Title Category :</label>
+                {["Movie", "Series", "Show"].map((cat) => (
+                  <div key={cat} className="flex gap-05">
+                    <input
+                      type="radio"
+                      id={cat.toLowerCase()}
+                      name="titlecategory"
+                      value={cat.toLowerCase()}
+                      checked={titlecategory === cat.toLowerCase()}
+                      onChange={(e) => setTitlecategory(e.target.value)}
+                    />
+                    <label htmlFor={cat.toLowerCase()}>{cat}</label>
+                  </div>
+                ))}
+              </div>
+
+              {/* movie category */}
+              <div className="moviecategory flex flex-sb flex-left">
+                <div className="w-50 flex flex-col flex-left mb-2">
+                  <label>Title Category :</label>
+                  {categories.map((cat) => (
+                    <div key={cat} className="flex gap-05">
+                      <input
+                        type="radio"
+                        id={cat.toLowerCase()}
+                        name="category"
+                        value={cat.toLowerCase()}
+                        checked={category === cat.toLowerCase()}
+                        onChange={(e) => setCategory(e.target.value)}
+                      />
+                      <label htmlFor={cat.toLowerCase()}>{cat}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* movie genre */}
+              <div className="moviecategory flex flex-sb flex-left">
+                <div className="w-50 flex flex-col flex-left mb-2">
+                  <label>Movie Genre :</label>
+                  {['Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Crime', 'Fantasy', 'Horror', 'Romance', 'Thriller', 'Science_Fiction'].map((genreOption) => (
+                    <label key={genreOption} className="flex gap-05">
+                      <input type="checkbox"
+                             value={genreOption.toLocaleLowerCase()}
+                             checked={genre.includes(genreOption.toLocaleLowerCase())}
+                             onChange={(e) => {
+                              const selectedGenre = e.target.value;
+                              setGenre((preGenre) => {
+                                if (preGenre.includes(selectedGenre)) {
+                                  return preGenre.filter((genre) => genre !== selectedGenre);
+                                } else {
+                                  return [...preGenre, selectedGenre];
+                                }
+                              })
+                             }}
+                      />
+                      {genreOption}
+                    </label>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+
+        {/* for save all data in mongoDB for submit button */}
+        
       </form>
     </>
   );
