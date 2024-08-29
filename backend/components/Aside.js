fe22da5450e903e5861b86116947b065618dd366
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-29 12:53:14 +0530

    Implemented sidebar navigation with dynamic links and icons
    - Added sidebar (`Aside.js`) with navigation links for Dashboard, Movies, Add Movie, and Draft sections.
    - Integrated `useRouter` for dynamic link activation based on the current route.
    - Included icons for each navigation item using `react-icons` for a visually appealing interface.
    - Implemented state management for handling active link highlighting and toggle behavior.
    - Enhanced user experience by dynamically updating the active link state upon page reload.

--- backend/components/Aside.js
+++ backend/components/Aside.js
@@ -1,9 +1,54 @@
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { BiCameraMovie, BiSolidCameraMovie } from "react-icons/bi";
+import { IoHomeSharp } from "react-icons/io5";
+import { MdOutlinePlaylistAdd } from "react-icons/md";
+import { RiDraftFill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
+import { PiSignInBold } from "react-icons/pi";
+
 
 
 export default function Aside() {
 
+    const router = useRouter();
+    const [clicked, setClicked] = useState(false);
+    const [activeLink, setActivelink] = useState('/');
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+    const handleLink = (link) => {
+        setActivelink(link);
+        setClicked(false);
+    }
+
+    useEffect(() => {
+        //update active link state when the page is reloaded
+        setActivelink(router.pathname);
+    }, [router.pathname])
 
     return <>
+    <div className="aside">
+        <div className="logo flex">
+            <BiCameraMovie/>
+            <Link href="/"><h1>MOVIES</h1></Link>
+        </div>
+        <ul className="mt-2">
+            <Link href="/" className={activeLink === '/' ? 'active' : ''} onClick={() => handleClick('/')}><li><div><IoHomeSharp/></div>Dashboard</li></Link>
+            <Link href="/movie" className={activeLink === '/movie' ? 'active' : ''} onClick={() => handleClick('/movie')}><li><div><BiSolidCameraMovie/></div>Movies</li></Link>
+            <Link href="/addmovie" className={activeLink === '/addmovie' ? 'active' : ''} onClick={() => handleClick('/addmovie')}><li><div><MdOutlinePlaylistAdd/></div>Add</li></Link>
+            <Link href="/draft" className={activeLink === '/draft' ? 'active' : ''} onClick={() => handleClick('/draft')}><li><div><RiDraftFill/></div>Draft</li></Link>
+        </ul>
+        <h3 className="mt-2">Account Pages</h3>
+        <ul className="mt-2">
+            <Link href="/profile" className={activeLink === '/profile' ? 'active' : ''} onClick={() => handleClick('/profile')}><li><div><FaUser/></div>Profile</li></Link>
+            <Link href="/auth" className={activeLink === '/auth' ? 'active' : ''} onClick={() => handleClick('/auth')}><li><div><PiSignInBold/></div>SignIn</li></Link>
+        </ul>
+    </div>
+
    
     </>
 }
