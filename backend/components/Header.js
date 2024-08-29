891fb848ae5b3cd9bd997d5808c3ed50a9e55591
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-29 12:55:24 +0530

    Enhanced Header functionality with API integration and search
    - Imported `useFetchData` hook to fetch movie data from the API in `Header.js`.
    - Implemented state management for search queries and search bar toggle.
    - Added a `VscThreeBars` icon for the mobile header bar.
    - Integrated movie filtering based on publish status within the `Header.js` component.

--- backend/components/Header.js
+++ backend/components/Header.js
@@ -1,11 +1,36 @@
+import useFectchData from "@/hooks/useFetchData"
+import { useEffect, useState } from "react"
+import { VscThreeBars } from "react-icons/vsc";
+
 
 
 export default function Header() {
 
+    //  Fwtch api
+    const {alldata, loading} = useFectchData('/api/getmovies');
 
+    const [searchQuery, setSearchQuery] = useState('');
+    const [openSearch, setOpenSearch] = useState(false);
 
+    //Filter published data
+    const publishedMovies = alldata.filter(ab => ab.status === "publish");
+    //search queary
 
     return <>
+
+    <header className="header">
+        <div className="flex flex-sb">
+            <div className="headerbar">
+                <VscThreeBars/>
+            </div>
+            <div className="searchheaderinput">
+                <input type="text" 
+                    placeholder="Search movie here"
+                    value={searchQuery}
+                />
+            </div>
+        </div>
+    </header>
 
     </>
 }
