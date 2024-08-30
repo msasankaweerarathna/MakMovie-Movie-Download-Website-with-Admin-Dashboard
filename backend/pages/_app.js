ea22eda420fc8f241f7c5cd6e0a1e8207c826b8e
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-30 22:55:38 +0530

    Import Header component

--- backend/pages/_app.js
+++ backend/pages/_app.js
@@ -1,10 +1,14 @@
 import Aside from "@/components/Aside";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <>
   <Aside/>
-  <Header/>
+  <div className="container">
+    <Header/>
+  </div>
+  
 
     <main>
       <Component {...pageProps} />
