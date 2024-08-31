be068b0a00ba36f3afc33ddb8d336b12133b9121
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-31 22:28:23 +0530

    Added profile page layout and functionality
    - Integrated `Head` component for setting the page title to "Profile page".
    - Designed profile section with user details including an image, name, and contact information.
    - Implemented input fields for phone number and email with default values and a save button.
    - Added a right sidebar section for account details with a logout button.
    - Incorporated `MdOutlineAccountCircle` icon for visual enhancement.

--- backend/pages/profile.js
+++ backend/pages/profile.js
@@ -1,5 +1,7 @@
 
 import Head from "next/head";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
 
 export default function profile() {
 
@@ -8,6 +10,44 @@ export default function profile() {
         <Head>
             <title>Profile page</title>
         </Head>
+
+        <div className="container">
+            <div className="profilesettings">
+                <div className="leftprofile_details flex">
+                <img src="/img/coder.png" alt="coder" />
+                    <div className="w-100">
+                        <div className="flex flex-sb flex-left mt-2">
+                            <h2>My Profile</h2>
+                            <h3>Vbm Coder <br />Web Designer</h3>
+                        </div>
+                        <div className="flex flex-sb mt-2">
+                            <h3>Phone</h3>
+                            <input type="text" defaultValue='+94 783471760 ' />
+                        </div>
+                        <div className="mt-3">
+                            <input type="email" defaultValue="manujaya77sasanka@gmail.com" />
+                        </div>
+                        <div className="flex flex-center w-100 mt-2">
+                            <button>Save</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="rightlogoutsec">
+                    <div className="topaccoutnbox">
+                        <h2 className="flex flex-sb">
+                            My Account 
+                            <MdOutlineAccountCircle/>
+                        </h2>
+                        <hr />
+                        <div className="flex flex-sb mt-1">
+                            <h3>Active Account <br /><span></span></h3>
+                            <button>Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       
     </>
 }
