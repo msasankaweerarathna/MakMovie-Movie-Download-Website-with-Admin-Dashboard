bb6f5515985a51167deddc2cc3fa6990e99e2fd3
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-01 00:56:39 +0530

    Added movie fetching logic to EditProduct component based on dynamic ID
    - Integrated axios to fetch movie data from the API using the dynamic ID from the URL query.
    - Implemented useEffect to trigger the API call when the ID is available.
    - Stored the fetched movie information in a local state using useState.

--- backend/pages/movies/edit/[...id].js
+++ backend/pages/movies/edit/[...id].js
@@ -1,8 +1,26 @@
 
+import axios from "axios";
 import Head from "next/head"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
 
 export default function EditProduct() {
 
+    const router = useRouter();
+
+    const { id } = router.query;
+
+    const [movieInfo, setMovieInfo] = useState(null);
+
+    useEffect(() => {
+        if (!id) {
+            return;
+        } else {
+            axios.get('/api/getmovies?id=' + id).then(response => {
+                setMovieInfo(response.data);
+            })
+        }
+    }, [])
 
     return <>
 
