b2b47430cb94b08e06931c3b42b131999701bda1
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-31 23:25:11 +0530

    Added Head and Image components to auth.js - Included `Head` for setting the page title. - Integrated `Image` from Next.js to display a profile image. - Updated the login interface with a welcome message and a Google login button. - Added a link to the LinkedIn profile for additional information.

--- backend/pages/auth.js
+++ backend/pages/auth.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Image from "next/image"
 
 export default function Auth() {
 
@@ -7,7 +8,16 @@ export default function Auth() {
         <Head>
             <title>Movie App | Backend</title>
         </Head>
-     
 
+        <div className="container">
+            <div className="loginfront flex flex-center">
+                <div className="loginbox flex flex-col">
+                    <Image src='/img/coder.png' width={250} height={250}/>
+                    <h1>Welcome Admin of the Makmovies</h1>
+                    <p>Visit our main Website <a href="https://www.linkedin.com/in/manujaya-sasanka-4a4284218/">Manujaya Sasanka</a></p>
+                    <button className="mt-2">Log in with Google</button>
+                </div>
+            </div>
+        </div>
     </>
 }
