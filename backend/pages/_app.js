a4aca4db48e5f8459f8b453b7933cafb8b1dabdd
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-01 23:55:03 +0530

    Integrated global components (Aside and Header) with session management into _app.js for consistent layout across pages

--- backend/pages/_app.js
+++ backend/pages/_app.js
@@ -1,18 +1,28 @@
 import Aside from "@/components/Aside";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <>
-  <Aside/>
-  <div className="container">
-    <Header/>
-  </div>
-  
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Aside />
+      </SessionProvider>
+      <div className="container">
+        <SessionProvider session={session}>
+          <Header />
+        </SessionProvider>
+      </div>
 
-    <main>
-      <Component {...pageProps} />
-    </main>
-
-  </>
+      <main>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </main>
+    </>
+  );
 }
