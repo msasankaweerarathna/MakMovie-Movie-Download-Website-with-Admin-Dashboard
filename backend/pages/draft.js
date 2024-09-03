d30d0a407735a633acc3bd2363adb50bad4b553f
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-03 12:37:14 +0530

    Added session management and loading state handling to draft.js - Imported `Loading` component to display during session verification. - Integrated `useSession` from `next-auth/react` to manage session data. - Implemented redirect to the login page if the user is not authenticated.

--- backend/pages/draft.js
+++ backend/pages/draft.js
@@ -5,8 +5,22 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { FcRating } from "react-icons/fc";
+import Loading from "@/components/Loading";
 
 export default function draft() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  // Chack if there's no active session and redirect to login page
+  if (!session) {
+    router.push("/auth");
+    return null; // return null or any loading indicator while redirecting
+  }
+
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage] = useState(7);
   const [searchQuery, setSearchQuery] = useState("");
@@ -42,94 +56,96 @@ export default function draft() {
     pageNumbers.push(i);
   }
 
-  return (
-    <>
-      <div className="container">
-        <div className="moviecards flex flex-col flex-left gap-2 w-100">
-          <div className="flex flex-sb w-100 movietitle">
-            <h2>List Of Draft Movies</h2>
-            <Link href="/addmovie">
-              <button>Add Movie</button>
-            </Link>
-          </div>
-          {loading ? (
-            <Spinner />
-          ) : (
-            <>
-              {draftMovies.length === 0 ? (
-                <h2 className="flex w-100 flex-center text-center">
-                  Empty Draft
-                </h2>
-              ) : (
-                <>
-                  {" "}
-                  {draftMovies.map((movie) => {
-                    return (
-                      <div className="moviecard" key={movie._id}>
-                        <img
-                          src={movie.bgposter || "/img/noimage.jpg"}
-                          alt="movie"
-                        />
-                        <div className="moviecardinfo">
-                          <div>
-                            <h3>{movie.title}</h3>
-                            <p>{movie.category}</p>
-                          </div>
-                          <Link href="/">asda</Link>
-                          <div>
-                            <FcRating /> {movie.rating}
-                          </div>
-                          <div className="flex gap-2 mt-2">
-                            <Link href={`/movies/edit/${movie._id}`}>
-                              <button>Update Movie</button>
-                            </Link>
-                            <Link href={`/movies/delete/${movie._id}`}>
-                              <button>Delete Movie</button>
-                            </Link>
+  if (session) {
+    return (
+      <>
+        <div className="container">
+          <div className="moviecards flex flex-col flex-left gap-2 w-100">
+            <div className="flex flex-sb w-100 movietitle">
+              <h2>List Of Draft Movies</h2>
+              <Link href="/addmovie">
+                <button>Add Movie</button>
+              </Link>
+            </div>
+            {loading ? (
+              <Spinner />
+            ) : (
+              <>
+                {draftMovies.length === 0 ? (
+                  <h2 className="flex w-100 flex-center text-center">
+                    Empty Draft
+                  </h2>
+                ) : (
+                  <>
+                    {" "}
+                    {draftMovies.map((movie) => {
+                      return (
+                        <div className="moviecard" key={movie._id}>
+                          <img
+                            src={movie.bgposter || "/img/noimage.jpg"}
+                            alt="movie"
+                          />
+                          <div className="moviecardinfo">
+                            <div>
+                              <h3>{movie.title}</h3>
+                              <p>{movie.category}</p>
+                            </div>
+                            <Link href="/">asda</Link>
+                            <div>
+                              <FcRating /> {movie.rating}
+                            </div>
+                            <div className="flex gap-2 mt-2">
+                              <Link href={`/movies/edit/${movie._id}`}>
+                                <button>Update Movie</button>
+                              </Link>
+                              <Link href={`/movies/delete/${movie._id}`}>
+                                <button>Delete Movie</button>
+                              </Link>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  })}
-                </>
-              )}
-            </>
-          )}
+                      );
+                    })}
+                  </>
+                )}
+              </>
+            )}
 
-          {draftMovies.length === 0 ? (
-            ""
-          ) : (
-            <div className="blogpagination">
-              <button
-                onClick={() => paginate(currentPage - 1)}
-                disabled={currentPage === 1}
-              >
-                Previous
-              </button>
-              {pageNumbers
-                .slice(
-                  Math.max(currentPage - 3, 0),
-                  Math.min(currentPage + 2, pageNumbers.length)
-                )
-                .map((number) => (
-                  <button
-                    key={number}
-                    onClick={() => paginate(number)}
-                    className={`${currentPage === number ? "active" : ""}`}
-                  >
-                    {number}
-                  </button>
-                ))}
-              <button
-                onClick={() => paginate(currentPage + 1)}
-                disabled={currentBlogs.length < perPage}
-              >
-                Next
-              </button>
-            </div>
-          )}
+            {draftMovies.length === 0 ? (
+              ""
+            ) : (
+              <div className="blogpagination">
+                <button
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </button>
+                {pageNumbers
+                  .slice(
+                    Math.max(currentPage - 3, 0),
+                    Math.min(currentPage + 2, pageNumbers.length)
+                  )
+                  .map((number) => (
+                    <button
+                      key={number}
+                      onClick={() => paginate(number)}
+                      className={`${currentPage === number ? "active" : ""}`}
+                    >
+                      {number}
+                    </button>
+                  ))}
+                <button
+                  onClick={() => paginate(currentPage + 1)}
+                  disabled={currentBlogs.length < perPage}
+                >
+                  Next
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
