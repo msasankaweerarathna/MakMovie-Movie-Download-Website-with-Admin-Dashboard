eeda9d2e908c303a4f3db1f8ee687591af2d4508
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-03 12:36:08 +0530

    Integrated session management and authentication buttons in auth.js - Imported `useSession`, `signIn`, and `signOut` from `next-auth/react` for session handling. - Used `useRouter` from `next/router` to manage routing based on session status. - Added a `Loading` component to handle loading state during session verification. - Implemented conditional rendering for login and logout buttons based on session status.

--- backend/pages/auth.js
+++ backend/pages/auth.js
@@ -1,23 +1,46 @@
-import Head from "next/head"
-import Image from "next/image"
+import Head from "next/head";
+import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import Loading from "@/components/Loading";
 
 export default function Auth() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-    
-    return <>
-        <Head>
-            <title>Movie App | Backend</title>
-        </Head>
+  if (status === "loading") {
+    return <Loading />;
+  }
 
-        <div className="container">
-            <div className="loginfront flex flex-center">
-                <div className="loginbox flex flex-col">
-                    <Image src='/img/coder.png' width={250} height={250}/>
-                    <h1>Welcome Admin of the Makmovies</h1>
-                    <p>Visit our main Website <a href="https://www.linkedin.com/in/manujaya-sasanka-4a4284218/">Manujaya Sasanka</a></p>
-                    <button className="mt-2">Log in with Google</button>
-                </div>
-            </div>
+  return (
+    <>
+      <Head>
+        <title>Movie App | Backend</title>
+      </Head>
+
+      <div className="container">
+        <div className="loginfront flex flex-center">
+          <div className="loginbox flex flex-col">
+            <Image src="/img/coder.png" width={250} height={250} />
+            <h1>Welcome Admin of the Makmovies</h1>
+            <p>
+              Visit our main Website{" "}
+              <a href="https://www.linkedin.com/in/manujaya-sasanka-4a4284218/">
+                Manujaya Sasanka
+              </a>
+            </p>
+            {session ? (
+              <button className="mt-2" onClick={signOut}>
+                Log Out Here
+              </button>
+            ) : (
+              <button className="mt-2" onClick={signIn}>
+                Log in with Google
+              </button>
+            )}
+          </div>
         </div>
+      </div>
     </>
+  );
 }
