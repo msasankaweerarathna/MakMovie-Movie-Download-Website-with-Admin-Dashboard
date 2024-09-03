6a31669e92ad19e7c7f4029eb1217639bfea5c31
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-03 12:34:35 +0530

    Added session management and loading state to Addmovie component - Imported `useSession` from `next-auth/react` to handle user sessions. - Integrated `Loading` component for displaying a loading state during session check. - Used `useRouter` from `next/router` to redirect unauthenticated users to the login page. - Ensured that the Addmovie component only renders for authenticated sessions.

--- backend/pages/addmovie.js
+++ backend/pages/addmovie.js
@@ -1,23 +1,37 @@
 import Movie from "@/components/Movie";
-
+import { useSession } from "next-auth/react";
+import Loading from "@/components/Loading";
+import { useRouter } from "next/router";
 
 export default function Addmovie() {
- 
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
+  if (status === "loading") {
+    return <Loading />;
+  }
 
-  return <>
-  <div className="addblogpage container">
-    <div className="blogsadd">
-      <div className="titledashboard w-100 flex flex-sb">
-        <div>
-          <h2>Add movie</h2>
-          <h3>ADMIN PANEL</h3>
+  // Chack if there's no active session and redirect to login page
+  if (!session) {
+    router.push("/auth");
+    return null; // return null or any loading indicator while redirecting
+  }
+
+  if (session) {
+    return (
+      <>
+        <div className="addblogpage container">
+          <div className="blogsadd">
+            <div className="titledashboard w-100 flex flex-sb">
+              <div>
+                <h2>Add movie</h2>
+                <h3>ADMIN PANEL</h3>
+              </div>
+            </div>
+            <Movie />
+          </div>
         </div>
-      </div>
-      <Movie/>
-    </div>
-  </div>
-
-  </>
-
+      </>
+    );
+  }
 }
