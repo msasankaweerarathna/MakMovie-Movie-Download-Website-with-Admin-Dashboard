fcd1212badaf6ada798e2ca147800a0a19f18b75
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-04 11:26:33 +0530

    Code cleaned

--- backend/pages/_app.js
+++ backend/pages/_app.js
@@ -1,17 +1,16 @@
 import Aside from "@/components/Aside";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
+export default function App({ Component, pageProps: { session, ...pageProps },
 }) {
   return (
     <>
       <SessionProvider session={session}>
         <Aside />
       </SessionProvider>
+      
       <div className="container">
         <SessionProvider session={session}>
           <Header />
