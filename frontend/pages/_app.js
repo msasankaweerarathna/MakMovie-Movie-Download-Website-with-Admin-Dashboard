4052d955225b2b11db50d2f4ea3bb52f6d1b2991
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-04 22:23:49 +0530

    Import Header component

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <>
+  <Header/>
     <main>
       <Component {...pageProps} />
     </main>
