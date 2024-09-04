5a6014084ab6da3f98aa51d680be8b655366897f
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-04 11:35:24 +0530

    Updated [...nextauth].js for potential optimizations and best practices - Reviewed and confirmed the configuration for Google authentication and MongoDB adapter integration.

--- backend/pages/api/auth/[...nextauth].js
+++ backend/pages/api/auth/[...nextauth].js
@@ -1,23 +1,17 @@
-import ClientPromise from "@/lib/mongodb"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
+export default NextAuth({
 
-export const authOptions = {
-  
   secret: process.env.NODE_ENV,
-  adapter: MongoDBAdapter(ClientPromise),
+  adapter: MongoDBAdapter(clientPromise),
 
-  
   providers: [
-    //Google Provider
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET
     }),
-
-  ],
-}
-
-export default NextAuth(authOptions)
+  ]
+})
