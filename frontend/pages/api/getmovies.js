f72d656e12260c8323bbf9bc1d08a37189d201b6
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-07 09:49:28 +0530

    Added GET request handling for fetching movies by various criteria
    - Connected MongoDB using `mongooseConnect` for database operations.
    - Implemented GET requests for fetching movies by ID, title, title category, and genre.
    - Reversed the order of results for category and genre to show the latest data.
    - Included error handling for unsupported methods with a 405 status response.

---
+++ frontend/pages/api/getmovies.js
@@ -0,0 +1,33 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Movie } from "@/models/Movie";
+
+
+export default async function handle(req, res){
+    const { method } = req;
+
+    //cpnnect mongodb database
+    await mongooseConnect();
+
+    // get movies data api
+    if (method === "GET") {
+        if (req.query?.id) {
+            //fetch a single movie by id
+            const movies = await Movie.findByID(req.query.id);
+            res.json(movies);
+        }else if (req.query?.title) {
+            // fetch movies by title for search funvtion
+            const title = await Movie.find({title: req.query.title});
+            res.json(title);
+        }else if (req.query?.titlecategory) {
+            // fetch movies by title category
+            const titlecategory = await Movie.find({titlecategory : req.query.titlecategory});
+            res.json(titlecategory.reverse()); // reverse for showing latest data
+        } else if (req.query?.genre) {
+            //fect movie by genre
+            const genre = await Movie.find({genre: req.query.genre});
+            res.json(genre.reverse()); // reverse for showing latest data
+        }
+    } else {
+        res.status(405).json({massage: "Method Not Allowed"})
+    }
+}
