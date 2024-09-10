aa807f532f44fac774f739a3d6634b8490364020
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-09-10 11:30:44 +0530

    Added Bollywood Movies Section with Fetching and Filtering Logic
    - Integrated data fetching from `/api/getmovies` using `useFetchData` hook.
    - Filtered published movies and applied an additional filter for Bollywood category.
    - Created Bollywood page layout with movie cards, each displaying an image, title, year, and ratings.
    - Added spinner to indicate loading state while data is being fetched.
    - Included `Link` component for movie details page routing.
    - Used FontAwesome icons for heart, eye, and star ratings in the movie cards.

--- frontend/pages/bollywood.js
+++ frontend/pages/bollywood.js
@@ -1,7 +1,21 @@
-
+import Link from "next/link";
+import useFectchData from "@/hooks/useFetchData";
+import Spinner from "@/components/Spinner";
 import Head from "next/head";
 
+import {FaEye, FaHeart, FaStar } from "react-icons/fa";
+
 export default function bollywood() {
+
+  // fetch data with usehook
+  const { alldata, loading } = useFectchData("/api/getmovies");
+
+  // filter for published bollywood required
+  const publishedData = alldata.filter((ab) => ab.status === "publish");
+
+  //filter data by bollywood
+  const bollywoodData = publishedData.filter((ab) => ab.category === 'bollywood');
+
    
     return <>
         <Head>
@@ -9,5 +23,55 @@ export default function bollywood() {
             <meta name="description" content="All the Web Series" />
         </Head>
       
+        <section className="genrenamesec">
+        <div className="genrename">
+          <h1>Bollywood</h1>
+          <p>
+            Discover the best in movies, series, and exclusive content at
+            MakMovies. Your go-to platform for endless entertainment across
+            genres. Stay tuned for new releases and updates!
+          </p>
+        </div>
+        </section>
+
+        <section className="genremoviesec">
+        <div className="genremovie">
+          {loading ? (<Spinner />) : ( <>
+              {bollywoodData.map((movie) => {
+                return ( <div className="mcard">
+                      <Link href={`/movies/${movie.slug}`}>
+                        <div className="cardimg">
+                          <img
+                            src={movie.smposter}
+                            alt="movie"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="contents">
+                          <h5>{movie.title}</h5>
+                          <h6>
+                            <span>{movie.year}</span>
+                            <div className="rate">
+                              <i className="cardfas">
+                                <FaHeart />
+                              </i>
+                              <i className="cardfas">
+                                <FaEye />
+                              </i>
+                              <i className="cardfas">
+                                <FaStar />
+                              </i>
+                              <h6>{movie.rating}</h6>
+                            </div>
+                          </h6>
+                        </div>
+                      </Link>
+                    </div>
+                );
+              })}
+            </>
+          )}
+        </div>
+        </section>
     </>
 }
